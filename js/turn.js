82dee81d883bfe97d369c25eb09b37a0bc16b360
Istarengwa <muller.lilian@outlook.fr>
2022-01-06 14:26:07 +0100

    add random target for ia and security dead for player target

--- js/turn.js
+++ js/turn.js
@@ -5,34 +5,37 @@ class Turn {
 
   init(player) {
     
-    
     console.log(player[0]);
     console.log(player[1]);
     console.log(player[2]);
     let attack = 0;
+
+    
+
     if (player[0].status != "loser") {
       attack = prompt(`Choose 1 to attack ${player[1].name}, Choose 2 to attack ${player[2].name}`);
-      player[0].dealDamage(player[attack]);
-    }
-    if (player[1].status != "loser") {
-      if (player[0].status != "loser") {
-        player[1].dealDamage(player[0]);
+      if (player[attack].hp <= 0) {
+        attack = prompt(`The chosen character is dead, choose 1 to attack ${player[1].name}, Choose 2 to attack ${player[2].name}`);
       }
       else {
-        player[1].dealDamage(player[2]);
-      }     
+        player[0].dealDamage(player[attack]);
+      }  
     }
-    if (player[2].status != "loser") {
-      if (player[1].status != "loser") {
-        player[2].dealDamage(player[1]);
-      }
-      else {
-       player[2].dealDamage(player[0]); 
+
+
+    for (let random = 1; random < player.length; random++) {
+
+      let targetRandom = player.filter(p => p !== player[random]);
+      let victimRandom = targetRandom[Math.floor(Math.random() * targetRandom.length)];
+      
+      console.log(`Ici ce trouve le test du choix du joueur random dans la boucle : ${player[random].name} et de sa target ${victimRandom.name}`)
+      if (player[random].status != "loser") {
+        if (victimRandom.status != "loser") {
+          player[random].dealDamage(victimRandom);
+        }
+        
       }
     }
-    
-    let test2 = player.filter(p => p.status == "loser").length;
-    console.log(test2);
 
     if (player.filter(p => p.status == "loser").length === player.length - 1) { //victory system
       for (let i=0; i < player.length; i++) {
@@ -42,7 +45,6 @@ class Turn {
       }
     }
     
-    console.log(test2);
     console.log(player[0]);
     console.log(player[1]);
     console.log(player[2]);
